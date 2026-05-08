b0b226c7cddfacecc909ec0a1399908e38575cee
Giuseppe Sica <62955768+Giuseppe0075@users.noreply.github.com>
2026-05-08 16:40:57 +0200

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    mapping = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
+        }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        
+
+        result = [""]
+
+        for digit in digits:
+            tmp = []
+            for currStr in result:
+                for char in self.mapping[digit]:
+                    tmp.append(currStr + char)
+            result = tmp
+
+        return result
